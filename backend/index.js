55a89e808620212ac5271bc06239f6be88912cb8
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-09-04 17:01:14 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./Config/db.js";
+import userRoutes from "./Routes/userRoutes.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api/users", userRoutes);
+app.listen(port, () => console.log(`Server Running on port ${port}`));
