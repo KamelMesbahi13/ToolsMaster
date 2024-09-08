5c5c2ba5180644aa9cc51b51dd8bb3244edc013f
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-09-08 18:45:25 +0100

    Adding The Category Component

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./Config/db.js";
 import userRoutes from "./Routes/userRoutes.js";
+import categoryRoutes from "./Routes/categoryRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -14,4 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+
 app.listen(port, () => console.log(`Server Running on port ${port}`));
